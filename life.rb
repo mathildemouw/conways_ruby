f10103b83aab6ef98f02a6b6564ca42603e67ca2
Mathilde Mouw <nihaomouw@mac.com>
2015-09-05 17:59:55 -0700

    Failing test for Game#next_turn and test framework

--- life.rb
+++ life.rb
@@ -1,34 +1,52 @@
 
 class Game
 
-	def initialize(rows*)
-		@board = (Array.new << rows)
+	def initialize(board)
+		@board = board
 	end
 
-	def next_turn(board)
+	def next_turn
 		
 	end
 end
 
+########################
 
-	
+def describe(class_or_method)
+	puts "describing #{class_or_method},"
+	yield
+end
+
+def it(description)
+	puts "if it works, it #{description}"
+	yield
+end
+
+def expect(behavior)
+ if behavior 
+ 	puts "and it does!"
+ else 
+ 	puts "but something went wrong"
+ end	
+end
+########################
 describe Game do
 
 	board = 	[[0,0,0],
-		 				 [c,0,0],
-		 				 [0,c,0],
-		 				 [0,c,c],
-		 				 [c,c,0]]
+		 				 [1,0,0],
+		 				 [0,1,0],
+		 				 [0,1,1],
+		 				 [1,1,0]]
 
 	next_turn_board =	[[0,0,0],
  				   					 [0,0,0],
- 				   					 [c,c,c],
- 				   					 [0,0,c],
- 				   					 [c,c,c]]
+ 				   					 [1,1,1],
+ 				   					 [0,0,1],
+ 				   					 [1,1,1]]
 
 	describe "#next_turn" do
 		it "returns the next board" do
-			expect(Game.new(board).next_turn).to eq next_turn_board
+			expect(Game.new(board).next_turn == next_turn_board)
 		end
 	end
 # live_cell_with_fewer_than_two_neighbors = board[1][0] #=> dies
@@ -36,5 +54,6 @@ describe Game do
 # live_cell_with_more_than_three_neighbors = board[3][1] #=> dies
 # dead_cell_with_three_live_neighbors = board[2][0] #=> dies
 end
+
 
 
